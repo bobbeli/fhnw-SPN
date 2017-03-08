4490cac59d52431eb13e256dfb6860c06fad7670
Dimitri Suter <dimitri@sayhello.ch>
2017-03-08 14:40:18 +0100

    added Tasks

--- src/ProgrammieraufgabeSPN.java
+++ src/ProgrammieraufgabeSPN.java
@@ -1,9 +1,24 @@
+import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
 
 public class ProgrammieraufgabeSPN {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// +3 Punkte. Please. bitte Geben
+		// Variable
+		
 	}
+	
+	
+	
+	// Method S Box und Inverse
+	
+	// Methode BitPermutation 
+	
+	// Methode X-OR Calc
+	
+	// Method Decript
 
 }
