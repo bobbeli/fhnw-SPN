58e96185c08e37e1257c8a16219b71897c8513af
Gzim Ukshini <gzim.ukshini@student.fhnw.ch>
2017-03-11 15:59:45 +0100

    Samschtig work @dimi

--- src/ProgrammieraufgabeSPN.java
+++ src/ProgrammieraufgabeSPN.java
@@ -1,24 +1,33 @@
 import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
 
 public class ProgrammieraufgabeSPN {
-	
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		SPNDecrypter test = new SPNDecrypter();
+
+		//Gegeben:
+		/*
+		 * SBox-Umwandlung
+		 * Bit-Permutation
+		 * Runden r
+		 * 
+		 */
+		
+		//xi = E((y−1 + i) mod 2l; k) ⊕ yi (i = 0; : : : n − 1)
+		
+		//Key s
+		//Länge muss 32 Bits
+		//0011 1010 1001 0100 1101 0110 0011 1111
 
 		// +3 Punkte. Please. bitte Geben
 		// Variable
 		
+		
+		
 	}
 	
-	
-	
-	// Method S Box und Inverse
-	
-	// Methode BitPermutation 
-	
-	// Methode X-OR Calc
-	
-	// Method Decript
 
 }
