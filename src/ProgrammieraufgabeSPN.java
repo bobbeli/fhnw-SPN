90f63543943370c9e8563d631e39a02c4ff40b16
Dimitri Suter <dimitri@sayhello.ch>
2017-03-15 08:27:18 +0100

    added Modi method

--- src/ProgrammieraufgabeSPN.java
+++ src/ProgrammieraufgabeSPN.java
@@ -6,17 +6,8 @@ public class ProgrammieraufgabeSPN {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		SPNDecrypter test = new SPNDecrypter();
-
-		//Gegeben:
-		/*
-		 * SBox-Umwandlung
-		 * Bit-Permutation
-		 * Runden r
-		 * 
-		 */
-		
-		//xi = E((y−1 + i) mod 2l; k) ⊕ yi (i = 0; : : : n − 1)
+		// Start Decrypt Chiffre Text
+		new SPNDecrypter();
 
 		
 		
--- src/SPNDecrypter.java
+++ src/SPNDecrypter.java
@@ -1,13 +1,25 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 
 public class SPNDecrypter {
 
 	String chiffreText = "00000100110100100000101110111000000000101000111110001110011111110110000001010001010000111010000000010011011001110010101110110000";
-	//String chiffreText = "10010101";
-	//public byte[] key = 00111010100101001101011000111111;
+	public int l = 16;
+	public String k = "00111010100101001101011000111111";
+	ArrayList<Integer> listOfSplittedBinary;
+	Map encryptedList;
+
+
 	public SPNDecrypter () {
-		splitInputText(chiffreText);
+		
+		splitInputText();
+		
+		System.out.println("dsfd");
+		
+		ctrModiDecrypt();
 	}
 	
 /////////////////////////////////////////////////////////////////////////////////////////////
@@ -19,29 +31,47 @@ public class SPNDecrypter {
 	 * 
 	 */
 /////////////////////////////////////////////////////////////////////////////////////////////	
+	
+	
+	
 	/*
 	 * Method splits chiffre text into blocks of length of 16
 	 * @param chiffreText string representation of the chiffre text
 	 * @return arraylist with splitted blocks
 	 * 
 	 */
-	public ArrayList<Integer> splitInputText(String chiffreText) {
+	public void splitInputText() {
 		
-		ArrayList<Integer> list = new ArrayList<Integer>();
+		listOfSplittedBinary = new ArrayList<>();
+		
 		String[] temp = chiffreText.split("(?<=\\G.{16})");
 		
 		for(String e: temp) {
-			list.add(Integer.parseInt(e, 2));
+			listOfSplittedBinary.add(Integer.parseInt(e, 2));
 		}
-		return list;
+		
 	}
 	
 	
-	//2)
-	//xi = E((y−1 + i) mod 2l; k) ⊕ yi (i = 0; : : : n − 1)
-	
+	/**
+	 * This Methdo handles Modi Decryption
+	 */
 	public void ctrModiDecrypt () {
+		encryptedList = new HashMap();
+		int i = 0;
+		int yi = listOfSplittedBinary.get(0);
+		listOfSplittedBinary.remove(0);
 		
+		for(int value : listOfSplittedBinary){
+			i++;
+			encryptedList.put(i, encrypt((value + i) % 2*l, Integer.parseInt(k,2)) ^ yi);
+			
+		}
+	}
+	
+	
+	public int encrypt(int chiffre, int k){
+		return 1;
 	}
 	
 	//3)
